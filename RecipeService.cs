aebc87534af471e32ebf0fcb6a2de9785fa53e37
Анна Помогаева <anutka-94@mail.ru>
2026-09-25 14:23:00 +0300

    Упростила метод удаления рецепта

--- RecipeService.cs
+++ RecipeService.cs
@@ -19,7 +19,7 @@ public class RecipeService
         int brewTimeSeconds = InputHelper.GetBrewTimeSeconds();
         Console.WriteLine("Введите комментарий");
         string comment = Console.ReadLine();
-    
+
         Recipe recipe = new Recipe();
         recipe.CoffeeName = coffeeName;
         recipe.CoffeeWeightGram = coffeeWeightGram;
@@ -33,138 +33,138 @@ public class RecipeService
         recipe.Comment = comment;
         return recipe;
     }
-    
+
     public static void EditRecipe(List<Recipe> recipes)
-{
-   RecipePrinter.PrintRecipeHeaders(recipes);
-    
-    int userNumberEdit;
-    do
     {
-        userNumberEdit = InputHelper.GetInt("Введите номер рецепта, чтобы изменить его");
-        if (userNumberEdit < 1 || userNumberEdit > recipes.Count)
-        {
-            Console.WriteLine("Такого рецепта нет, попробуйте еще раз");
-        }
-        
-    } while (userNumberEdit < 1 || userNumberEdit > recipes.Count);
+        RecipePrinter.PrintRecipeHeaders(recipes);
 
-    Recipe originalRecipe = recipes[userNumberEdit - 1];
-    Recipe recipeToEdit = new Recipe();
-    
-    recipeToEdit.BrewMethod = originalRecipe.BrewMethod;
-    recipeToEdit.Roaster = originalRecipe.Roaster;
-    recipeToEdit.CoffeeName = originalRecipe.CoffeeName;
-    recipeToEdit.RoastLevel = originalRecipe.RoastLevel;
-    recipeToEdit.CoffeeWeightGram = originalRecipe.CoffeeWeightGram;
-    recipeToEdit.Temperature = originalRecipe.Temperature;
-    recipeToEdit.GrindSize = originalRecipe.GrindSize;
-    recipeToEdit.WaterWeightGram = originalRecipe.WaterWeightGram;
-    recipeToEdit.BrewTimeSecond = originalRecipe.BrewTimeSecond;
-    recipeToEdit.Comment = originalRecipe.Comment;
-
-    int editMenuChoise;
-    do
-    {
-        RecipePrinter.PrintRecipe(recipeToEdit);
-        Console.WriteLine("");
-        Console.WriteLine("Введите номер поля, чтобы изменить его");
-        RecipePrinter.PrintEditMenu();
-        editMenuChoise = InputHelper.GetInt("");
-        
-        switch (editMenuChoise)
-        {
-            case 1:
-                recipeToEdit.BrewMethod = InputHelper.GetBrewMethod();
-                break;
-            case 2:
-                Console.WriteLine("Введите название обжарщика");
-                string roaster = Console.ReadLine();
-                recipeToEdit.Roaster = roaster;
-                break;
-            case 3:
-                Console.WriteLine("Введите название кофе");
-                string newCoffeeName= Console.ReadLine();
-                recipeToEdit.CoffeeName = newCoffeeName;
-                break;
-            case 4:
-                recipeToEdit.RoastLevel = InputHelper.GetRoastLevel();
-                break;
-            case 5:
-                double newCoffeeWeightGram = InputHelper.GetDouble("Введите вес зерна в граммах");
-                recipeToEdit.CoffeeWeightGram = newCoffeeWeightGram;
-                break;
-            case 6:
-                int newTemperature = InputHelper.GetInt("Введите температуру воды");
-                recipeToEdit.Temperature = newTemperature;
-                break;
-            case 7:
-                double newGrindSize = InputHelper.GetDouble("Введите значение помола");
-                recipeToEdit.GrindSize = newGrindSize;
-                break;
-            case 8:
-                double newWaterWeightGram = InputHelper.GetDouble("Введите количество воды в проливе в граммах");
-                recipeToEdit.WaterWeightGram = newWaterWeightGram;
-                break;
-            case 9:
-                recipeToEdit.BrewTimeSecond = InputHelper.GetBrewTimeSeconds();
-                break;
-            case 10:
-                Console.WriteLine("Введите комментарий");
-                string comment = Console.ReadLine();
-                recipeToEdit.Comment = comment;
-                break;
-            case 11:
-                recipes[userNumberEdit - 1] = recipeToEdit;
-                Console.WriteLine("Измененный рецепт:");
-                RecipePrinter.PrintRecipe(recipeToEdit);
-                break;
-            case 12:
-                Console.WriteLine("Изменения отменены");
-                break;
-        }
-    } while (editMenuChoise != 11 && editMenuChoise != 12);
-    
-}
-   public static void DeleteRecipe(List<Recipe> recipes)
-    {
-        int userNumberDelete;
-        int deleteConfirmation;
+        int userNumberEdit;
         do
         {
-            do
+            userNumberEdit = InputHelper.GetInt("Введите номер рецепта, чтобы изменить его");
+            if (userNumberEdit < 1 || userNumberEdit > recipes.Count)
             {
-                RecipePrinter.PrintRecipeHeaders(recipes);
-                userNumberDelete = InputHelper.GetInt("Введите номер рецепта для удаления. Чтобы вернуться назад, нажмите 0");
-                if (userNumberDelete < 0 || userNumberDelete > recipes.Count)
-                {
-                    Console.WriteLine("Такого рецепта нет, попробуйте еще раз");
-                }
-                else if (userNumberDelete == 0)
-                {
-                    return;
-                }
-            
-            } while (userNumberDelete <0 || userNumberDelete > recipes.Count);
+                Console.WriteLine("Такого рецепта нет, попробуйте еще раз");
+            }
 
-            do
+        } while (userNumberEdit < 1 || userNumberEdit > recipes.Count);
+
+        Recipe originalRecipe = recipes[userNumberEdit - 1];
+        Recipe recipeToEdit = new Recipe();
+
+        recipeToEdit.BrewMethod = originalRecipe.BrewMethod;
+        recipeToEdit.Roaster = originalRecipe.Roaster;
+        recipeToEdit.CoffeeName = originalRecipe.CoffeeName;
+        recipeToEdit.RoastLevel = originalRecipe.RoastLevel;
+        recipeToEdit.CoffeeWeightGram = originalRecipe.CoffeeWeightGram;
+        recipeToEdit.Temperature = originalRecipe.Temperature;
+        recipeToEdit.GrindSize = originalRecipe.GrindSize;
+        recipeToEdit.WaterWeightGram = originalRecipe.WaterWeightGram;
+        recipeToEdit.BrewTimeSecond = originalRecipe.BrewTimeSecond;
+        recipeToEdit.Comment = originalRecipe.Comment;
+
+        int editMenuChoise;
+        do
+        {
+            RecipePrinter.PrintRecipe(recipeToEdit);
+            Console.WriteLine("");
+            Console.WriteLine("Введите номер поля, чтобы изменить его");
+            RecipePrinter.PrintEditMenu();
+            editMenuChoise = InputHelper.GetInt("");
+
+            switch (editMenuChoise)
             {
-                Recipe recipeToDelete = recipes[userNumberDelete - 1];
-                deleteConfirmation =
-                    InputHelper.GetInt(
-                        $"Вы действительно хотите удалить {recipeToDelete.CoffeeName}? Если да, нажмите 1. Если нет, нажмите 0");
-                if (deleteConfirmation == 1)
-                {
-                    recipes.Remove(recipeToDelete);
-                    Console.WriteLine("Рецепт удален");
-                }
-                else if (deleteConfirmation != 0)
-                {
-                    Console.WriteLine("Вы ввели некорректное значение");
-                }
-            } while (deleteConfirmation != 0 && deleteConfirmation !=1);
+                case 1:
+                    recipeToEdit.BrewMethod = InputHelper.GetBrewMethod();
+                    break;
+                case 2:
+                    Console.WriteLine("Введите название обжарщика");
+                    string roaster = Console.ReadLine();
+                    recipeToEdit.Roaster = roaster;
+                    break;
+                case 3:
+                    Console.WriteLine("Введите название кофе");
+                    string newCoffeeName = Console.ReadLine();
+                    recipeToEdit.CoffeeName = newCoffeeName;
+                    break;
+                case 4:
+                    recipeToEdit.RoastLevel = InputHelper.GetRoastLevel();
+                    break;
+                case 5:
+                    double newCoffeeWeightGram = InputHelper.GetDouble("Введите вес зерна в граммах");
+                    recipeToEdit.CoffeeWeightGram = newCoffeeWeightGram;
+                    break;
+                case 6:
+                    int newTemperature = InputHelper.GetInt("Введите температуру воды");
+                    recipeToEdit.Temperature = newTemperature;
+                    break;
+                case 7:
+                    double newGrindSize = InputHelper.GetDouble("Введите значение помола");
+                    recipeToEdit.GrindSize = newGrindSize;
+                    break;
+                case 8:
+                    double newWaterWeightGram = InputHelper.GetDouble("Введите количество воды в проливе в граммах");
+                    recipeToEdit.WaterWeightGram = newWaterWeightGram;
+                    break;
+                case 9:
+                    recipeToEdit.BrewTimeSecond = InputHelper.GetBrewTimeSeconds();
+                    break;
+                case 10:
+                    Console.WriteLine("Введите комментарий");
+                    string comment = Console.ReadLine();
+                    recipeToEdit.Comment = comment;
+                    break;
+                case 11:
+                    recipes[userNumberEdit - 1] = recipeToEdit;
+                    Console.WriteLine("Измененный рецепт:");
+                    RecipePrinter.PrintRecipe(recipeToEdit);
+                    break;
+                case 12:
+                    Console.WriteLine("Изменения отменены");
+                    break;
+            }
+        } while (editMenuChoise != 11 && editMenuChoise != 12);
 
-        } while (deleteConfirmation == 0);
     }
-    
+
+    public static void DeleteRecipe(List<Recipe> recipes)
+    {
+        int userNumberDelete;
+        do
+        {
+            RecipePrinter.PrintRecipeHeaders(recipes);
+            userNumberDelete =
+                InputHelper.GetInt("Введите номер рецепта для удаления. Чтобы вернуться назад, нажмите 0");
+            if (userNumberDelete == 0)
+            {
+                return;
+            }
+
+            if (userNumberDelete < 1 || userNumberDelete > recipes.Count)
+            {
+                Console.WriteLine("Такого рецепта нет, попробуйте еще раз");
+            }
+        } while (userNumberDelete < 1 || userNumberDelete > recipes.Count);
+
+        Recipe recipeToDelete = recipes[userNumberDelete - 1];
+        int deleteConfirmation;
+
+        do
+        {
+            deleteConfirmation =
+                InputHelper.GetInt(
+                    $"Вы действительно хотите удалить {recipeToDelete.CoffeeName}? \n Если да, нажмите 1. Если нет, нажмите 0");
+            if (deleteConfirmation == 1)
+            {
+                recipes.Remove(recipeToDelete);
+                Console.WriteLine("Рецепт удален");
+                return;
+            }
+
+            if (deleteConfirmation != 0)
+            {
+                Console.WriteLine("Введите 1 или 0");
+            }
+        } while (deleteConfirmation != 0);
+    }
 }
